5f13cb6283227396f86d357e76e221421ddfe51e
liverbird <ehdgns07@gmail.com>
2022-10-04 16:43:34 +0900

    end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import getData from "./getData";
 
 function App() {
+
+    // const text = async (){
+    //     axios.get(url).then((Response) =>
+    //         Response.data).catch((error) => console.log(error));
+    // }
+    const text = async function getData() {
+        const url = "/dev.do";
+
+        await axios.get(url).then(Response => document.getElementById("id").innerHTML = Response.data.dev);
+
+    }
+
+    window.onload = text();
+
+
   return (
     <div className="App">
       <header className="App-header">
+          <p id="id"></p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
